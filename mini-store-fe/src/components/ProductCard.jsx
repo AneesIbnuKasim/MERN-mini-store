db8df9adfccd352a2b9d47d98fa1dc185ff65ca2
Anees <Aneesdropz@gmail.com>
2025-10-08 14:43:49 +0530

    implemented rating on front rnd ui product display card

--- mini-store-fe/src/components/ProductCard.jsx
+++ mini-store-fe/src/components/ProductCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import useProduct from '../hooks/useProduct';
+import { FaStar, FaRegStar, FaStarHalfAlt } from "react-icons/fa";
 
 function ProductCard() {
   const {state} = useProduct()
   const {products} = state
+  const rating = 3
 
   const API_BASE_URL = import.meta.env.BACKEND_API_BASE_URL || 'http://localhost:3000';
 
@@ -30,6 +32,23 @@ function getImageUrl(imagePath) {
                   <p  className='text-gray-500 text-sm sm:text-md'>{product.brand}</p>
                 <p className='text-md sm:text-lg'>{product.title}</p>
                 <p className='text-sm text-gray-700 overflow-hidden text-ellipsis [display:-webkit-box] [-webkit-line-clamp:2] [-webkit-box-orient:vertical]'>{product.description}</p>
+    {/* ---------------------------- star rating ------------------------------ */}
+     <div className='flex my-2 items-center gap-1'>
+      {[...Array(5)].map((_, index) => {
+        if (product.rating >= index + 1) {
+          // full star
+          return <FaStar key={index} size={24} className='text-yellow-500' />;
+        } else if (product.rating > index && product.rating < index + 1) {
+          // half star
+          return <FaStarHalfAlt key={index} size={24} className='text-yellow-500' />;
+        } else {
+          // empty star
+          return <FaRegStar key={index} size={24} className='text-yellow-500' />;
+        }
+      })}
+      <p>({product.rating})</p>
+    </div>
+
                 <h4 className='mt-1'>${product.price}</h4>
                 <button className='bg-[#edb228] mt-2 py-1 px-3 rounded-xl' >Add to cart</button>
                 </div>
